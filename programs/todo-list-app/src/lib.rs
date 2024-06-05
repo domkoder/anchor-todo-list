2ebf6cde02ace1de4bf39593b370a35fafee9a24
Nandom Alfred <nandommamdam@gmail.com>
2024-06-05 12:34:23 +0100

    deployed my program to localnet

--- programs/todo-list-app/src/lib.rs
+++ programs/todo-list-app/src/lib.rs
@@ -7,45 +7,21 @@ pub mod todo_list_app {
     use super::*;
 
     pub fn adding_task(ctx: Context<AddingTask>, text: String) -> Result<()> {
-        // let task = &mut ctx.accounts.task;
-        // let author = &ctx.accounts.author; // The `author` account
-        // let clock = Clock::get().unwrap(); // Getting the current timestamp
-        
-        // if text.chars().count() > 400 {
-        //     return Err(ErrorCode::TextTooLong.into());
-        // }
-        
-        // task.author = *author.key;
-        // task.is_done = false;
-        // task.created_at = clock.unix_timestamp;
-        // task.updated_at = clock.unix_timestamp;
-        // task.text = text;
+        let task = &mut ctx.accounts.task;
+        let author = &mut ctx.accounts.author;
+        let clock = Clock::get().unwrap();
+        if text.chars().count() > 400 {
+            return Err(ErrorCode::TextTooLong.into())
+        }
+        task.author = * author.key;
+        task.is_done = false;
+        task.created_at = clock.unix_timestamp;
+        task.updated_at = clock.unix_timestamp;
+        task.text = text;
         Ok(())
-
     }
 
-    // pub fn updating_task(ctx: Context<UpdatingTask>, is_done: bool) -> Result<()> {
-    //     let task = &mut ctx.accounts.task;
-    //     let author = &ctx.accounts.author; // The `author` account
-    //     let clock = Clock::get().unwrap(); // Getting the current timestamp
-        
-    //     task.author = *author.key;
-    //     task.is_done = is_done;
-    //     task.updated_at = clock.unix_timestamp;
-    //     Ok(())
-       
-    // }
 
-    // pub fn deleting_task(ctx: Context<DeletingTask>) -> Result<()> {
-    //     let task = &mut ctx.accounts.task;
-    //     let author = &ctx.accounts.author; // The `author` account
-    //     let clock = Clock::get().unwrap(); // Getting the current timestamp
-        
-    //     task.author = *author.key;
-    //     task.is_done = true;
-    //     task.updated_at = clock.unix_timestamp;
-    //     Ok(())
-    // }
 
 
 
@@ -54,53 +30,40 @@ pub mod todo_list_app {
 
 #[derive(Accounts)]
 pub struct AddingTask<'info> {
+    #[account(init, payer = author, space = Task::LEN)]
     pub task: Account<'info, Task>,
+    #[account(mut)]
     pub author: Signer<'info>,
     pub system_program: Program<'info, System>
 }
 
-// #[derive(Accounts)]
-// pub struct UpdatingTask<'info> {
-//     #[account(mut, has_one = author)]
-//     pub task: Account<'info, Task>,
-//     pub author: Signer<'info>,
-// }
-
-// #[derive(Accounts)]
-// pub struct DeletingTask<'info> {
-//     #[account(mut, has_one = author)]
-//     pub task: Account<'info, Task>,
-//     pub author: Signer<'info>,
-// }
-
 
 #[account]
 pub struct Task {
-    pub author: Pubkey,  // The account that owns the task
-    pub is_done: bool,   // Whether the task is done or not
-    pub text: String,    // The text of the task
-    pub created_at: i64, // The timestamp when the task was created
-    pub updated_at: i64, // The timestamp when the task was last updated
+    pub author: Pubkey,
+    pub is_done: bool,
+    pub text: String,
+    pub created_at: i64,
+    pub updated_at: i64,
 }
 
 const DISCRIMINATOR: usize = 8;
 const PUBLIC_KEY_LENGTH: usize = 32;
 const BOOL_LENGTH: usize = 1;
-const TEXT_LENGTH: usize = 4 + 400 * 4; // 400 chars
+const TEXT_LENGTH: usize = 4 + 400 * 4;
 const TIMESTAMP_LENGTH: usize = 8;
 
 impl Task {
-    const LEN: usize = DISCRIMINATOR + // discriminator
-        PUBLIC_KEY_LENGTH + // author
-        BOOL_LENGTH + // is_done
-        TEXT_LENGTH +  // text
-        TIMESTAMP_LENGTH + // created_at
-        TIMESTAMP_LENGTH; // updated_at
+    const LEN: usize = DISCRIMINATOR +
+        PUBLIC_KEY_LENGTH +
+        BOOL_LENGTH +
+        TEXT_LENGTH +
+        TIMESTAMP_LENGTH +
+        TIMESTAMP_LENGTH;
 }
 
-
-// #[error_code]
-// pub enum ErrorCode {
-//     #[msg("The text is too long")]
-//     TextTooLong,
-// }
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The text is too long")]
+    TextTooLong,
+}
